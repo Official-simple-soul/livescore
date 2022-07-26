4fb3a2cd619aa89b79d19655fbc5c065bad19a40
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-07-26 20:12:22 +0100

    new

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ function getData() {
 		}
 	};
 	
-	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2021-01-29', options)
+	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all', options)
 		.then(response => response.json())
 		.then(data => {
 			// console.log(data);
 			let fixtures = data['response']
-			// console.log(fixtures);
+			console.log(fixtures);
 
 			let fixture = fixtures.map(data => {
 				            return `
@@ -67,7 +67,7 @@ getData();
 
 // get live data
 
-live.addEventListener('click', () => {
+all.addEventListener('click', () => {
 	console.log('live');
 	const options = {
 		method: 'GET',
@@ -77,7 +77,7 @@ live.addEventListener('click', () => {
 		}
 	};
 	
-	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all', options)
+	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2022-07-26', options)
 		.then(response => response.json())
 		.then(data => {
 			
@@ -116,15 +116,23 @@ live.addEventListener('click', () => {
 					}
 					);
 					
-		
+		all.classList.add('actives');
+		live.classList.remove('actives');
+		coming.classList.remove('actives');
+		finished.classList.remove('actives');
 })
 
 
 // reload page 
 
-all.addEventListener('click', () => {
+live.addEventListener('click', () => {
 	console.log('all');
 	getData();
+
+	all.classList.remove('actives');
+	live.classList.add('actives');
+	coming.classList.remove('actives');
+	finished.classList.remove('actives');
 }
 )
 
@@ -172,57 +180,67 @@ finished.addEventListener('click', () => {
 				        document.querySelector('.row').innerHTML = fixture;
 		})
 
+		all.classList.remove('actives');
+		live.classList.remove('actives');
+		coming.classList.remove('actives');
+		finished.classList.add('actives');
+
 	});
 
 
 // upcoming fixtures		
 
-	coming.addEventListener('click', () => {
-		const options = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': 'aa9fe9fa58msh53c01e7eb8c954ap12c810jsn1571c226b0f4',
-				'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-			}
-		};
-		
-		fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?next=50', options)
-			.then(response => response.json())
-			.then(data => {
-				// console.log(response);
-				// console.log(data);
-			let fixtures = data['response']
-			// console.log(fixtures);
+coming.addEventListener('click', () => {
+	// window.alert('coming soon');
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'aa9fe9fa58msh53c01e7eb8c954ap12c810jsn1571c226b0f4',
+			'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+		}
+	};
+	
+	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?next=50', options)
+		.then(response => response.json())
+		.then(data => {
+			// console.log(response);
+			// console.log(data);
+		let fixtures = data['response']
+		// console.log(fixtures);
 
-			let fixture = fixtures.map(data => {
-				            return `
-								<div class="col-md-2 col-2 border-top border-2 d-flex  align-items-center">
-										<p class="time-first  mx-4">${data.fixture.status.elapsed}'</p>
+		let fixture = fixtures.map(data => {
+						return `
+							<div class="col-md-2 col-2 border-top border-2 d-flex  align-items-center">
+									<p class="time-first  mx-4">${data.fixture.status.elapsed}'</p>
+							</div>
+							<div class="col-md-10 col-10 border-top border-2">
+								<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-start align-items-center my-1">
+										<img src="${data.teams.home.logo}" alt="" class="home-logo" width="40px">
+										<h5 class="home text-primary mx-3">${data.teams.home.name}</h5>
+									</div>	
+									<h5 class="home-score">${data.goals.home}</h5>
 								</div>
-								<div class="col-md-10 col-10 border-top border-2">
-									<div class="d-flex justify-content-between align-items-center my-1">
-										<div class="d-flex justify-content-start align-items-center my-1">
-											<img src="${data.teams.home.logo}" alt="" class="home-logo" width="40px">
-											<h5 class="home text-primary mx-3">${data.teams.home.name}</h5>
-										</div>	
-										<h5 class="home-score">${data.goals.home}</h5>
+								<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-start align-items-center my-1">
+										<img src="${data.teams.away.logo}" alt="" class="away-logo" width="40px">
+										<h5 class="away text-success mx-3">${data.teams.away.name}</h5>
 									</div>
-									<div class="d-flex justify-content-between align-items-center my-1">
-										<div class="d-flex justify-content-start align-items-center my-1">
-											<img src="${data.teams.away.logo}" alt="" class="away-logo" width="40px">
-											<h5 class="away text-success mx-3">${data.teams.away.name}</h5>
-										</div>
-										<h5 class="away-score">${data.goals.away}</h5>
-									</div>
+									<h5 class="away-score">${data.goals.away}</h5>
 								</div>
-							`;
-				        }).join('');
-						
-				        document.querySelector('.row').innerHTML = fixture;
-						// console.log(data.goals.home)
-			})
+							</div>
+						`;
+					}).join('');
+					
+					document.querySelector('.row').innerHTML = fixture;
+					// console.log(data.goals.home)
+		})
 
-	});
+		all.classList.remove('actives');
+		live.classList.remove('actives');
+		coming.classList.add('actives');
+		finished.classList.remove('actives');
+});
 
 	
 
