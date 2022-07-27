54a2707127df5349520fe707a1a33ba0ea787e6a
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-07-27 18:47:44 +0100

    new

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ let live = document.querySelector('.live');
 let all = document.querySelector('.all');
 let finished = document.querySelector('.finish');
 let coming = document.querySelector('.coming');
+let england = document.querySelector('.england');
+let spain = document.querySelector('.spain');
+let italy = document.querySelector('.italy');
+let germany = document.querySelector('.germany');
+let france = document.querySelector('.france');
+let holland = document.querySelector('.holland');
+
+
 
 function getData() {
 	const options = {
@@ -242,6 +250,53 @@ coming.addEventListener('click', () => {
 		finished.classList.remove('actives');
 });
 
+
+// english premier league
+
+england.addEventListener('click', () => {
+	window.alert('coming soon');
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'aa9fe9fa58msh53c01e7eb8c954ap12c810jsn1571c226b0f4',
+			'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+		}
+	};
 	
+	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?league=1&season=2022', options)
+		.then(response => response.json())
+		.then(data => {
+			console.log(response);
+			// console.log(data);
+		let fixtures = data['response']
+		// console.log(fixtures);
+
+		let fixture = fixtures.map(data => {
+						return `
+							<div class="col-md-2 col-2 border-top border-2 d-flex  align-items-center">
+									<p class="time-first  mx-4">${data.fixture.status.elapsed}'</p>
+							</div>
+							<div class="col-md-10 col-10 border-top border-2">
+								<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-start align-items-center my-1">
+										<img src="${data.teams.home.logo}" alt="" class="home-logo" width="40px">
+										<h5 class="home text-primary mx-3">${data.teams.home.name}</h5>
+									</div>
+									<h5 class="home-score">${data.goals.home}</h5>
+								</div>
+								<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-start align-items-center my-1">
+										<img src="${data.teams.away.logo}" alt="" class="away-logo" width="40px">
+										<h5 class="away text-success mx-3">${data.teams.away.name}</h5>
+									</div>
+									<h5 class="away-score">${data.goals.away}</h5>
+								</div>
+							</div>
+						`;
+					}).join('');
+					document.querySelector('.row').innerHTML = fixture;
+					// console.log(data.goals.home)
+		})
+	});
 
 	
