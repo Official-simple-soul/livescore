d1ca96f7efe63233865d4aaf38e82b964ca64aab
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-12 13:05:40 +0100

    new

--- js/app.js
+++ js/app.js
@@ -8,6 +8,21 @@ menuBtn.addEventListener('click', function() {
 	rowling.classList.toggle('show');
 } );
 
+// select one navbar item
+function selectNavbarItem() {
+	let navbarItems = document.querySelectorAll('.all');
+	navbarItems.forEach(function(item) {
+		item.addEventListener('click', function() {
+			navbarItems.forEach(function(item) {
+				item.classList.remove('active');
+			} );
+			this.classList.add('active');
+		} );
+	} );
+}
+selectNavbarItem();
+
+// Api key
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,6 +31,7 @@ const options = {
 	}
 };
 
+// get leagues
 function getData() {
 	
 	fetch('https://api-football-v1.p.rapidapi.com/v3/leagues', options)
@@ -47,7 +63,6 @@ function getData() {
 }
 getData();
 
-
 // get fixtures
 function getFixtures() {
 	
@@ -55,25 +70,42 @@ function getFixtures() {
 		.then(response => response.json())
 		.then(data => {
 			let fixtures = data['response']
-			// console.log(fixtures)
+
 			let fixture = fixtures.map(data => {
+				// converting timestamp to local time
+				let date = new Date(data.fixture.timestamp * 1000);
+				let hours = date.getHours();
+				let minutes = "0" + date.getMinutes();
+				let formattedTime = hours + ':' + minutes.substr(-2);
+
+				// add am or pm
+				let ampm = hours >= 12 ? 'pm' : 'am';
+				formattedTime += ampm;
+				
 				            return `
 								<div class="col-md-12 col-12 cursour">
-									<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-between align-items-center my-0">
 										<div class="d-flex justify-content-start align-items-center my-1">
 											<img src="${data.teams.home.logo}" alt="" class="home-logo" width="30px">
 											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.home.name}</h5>
 										</div>	
-										<h5 class="home-score" style="font-size: 13px">${data.score.fulltime.home}</h5>
+										<h5 class="home-score" style="font-size: 14px">${data.score.fulltime.home}</h5>
+									</div>
+								</div>
+								<div class="col-md-12 col-12">
+									<div class="d-flex justify-content-between align-items-center my-0">
+
+										<p class="time text-danger my-0 mx-3" style="font-size: 11px">${formattedTime}</p>
+										<p class="time text-danger my-0 mx-3" style="font-size: 12px">${data.fixture.status.short}</p>
 									</div>
 								</div>
 								<div class="col-md-12 col-12  cursour border-bottom border-3">
-									<div class="d-flex justify-content-between align-items-center my-1">
+									<div class="d-flex justify-content-between align-items-center my-0">
 										<div class="d-flex justify-content-start align-items-center my-1">
 											<img src="${data.teams.away.logo}" alt="" class="home-logo" width="30px">
 											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.away.name}</h5>
 										</div>	
-										<h5 class="away-score" style="font-size: 13px">${data.score.fulltime.away}</h5>
+										<h5 class="away-score" style="font-size: 14px">${data.score.fulltime.away}</h5>
 									</div>
 								</div>
 
@@ -82,12 +114,25 @@ function getFixtures() {
 							document.querySelector('.rowling-fixture').innerHTML = fixture;
 
 							// fixtures conditions
+							let score = document.querySelectorAll('.home-score');
+							score.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
+							let score2 = document.querySelectorAll('.away-score');
+							score2.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
 
 							})
 
 						
-}	
-getFixtures();	
+}
 
 // get live fixtures
 function getLiveFixtures() {
@@ -97,6 +142,14 @@ function getLiveFixtures() {
 		.then(data => {
 			let fixtures = data['response']
 			let fixture = fixtures.map(data => {
+
+				// converting period to minutes
+				let period = data.fixture.periods.first;
+				let period2 = data.fixture.periods.second;
+				let plus = period + period2;
+				let minutes = Math.floor(plus / 60000000);
+
+
 				            return `
 								<div class="col-md-12 col-12 cursour">
 									<div class="d-flex justify-content-between align-items-center my-1">
@@ -104,7 +157,13 @@ function getLiveFixtures() {
 											<img src="${data.teams.home.logo}" alt="" class="home-logo" width="30px">
 											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.home.name}</h5>
 										</div>	
-										<h5 class="home-score-live text-light" style="font-size: 13px">${data.goals.home}</h5>
+										<h5 class="home-score-live text-danger fw-bold" style="font-size: 13px">${data.goals.home}</h5>
+									</div>
+								</div>
+								<div class="col-md-12 col-12">
+									<div class="d-flex justify-content-between align-items-center my-0">
+										<p class="time text-danger my-0 mx-3" style="font-size: 11px">live</p>
+										<p class="time text-danger my-0 mx-3" style="font-size: 11px">${minutes}</p>
 									</div>
 								</div>
 								<div class="col-md-12 col-12  cursour border-bottom border-3">
@@ -113,16 +172,32 @@ function getLiveFixtures() {
 											<img src="${data.teams.away.logo}" alt="" class="home-logo" width="30px">
 											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.away.name}</h5>
 										</div>	
-										<h5 class="away-score-live text-light" style="font-size: 13px">${data.goals.away}</h5>
+										<h5 class="away-score-live text-danger fw-bold" style="font-size: 13px">${data.goals.away}</h5>
 									</div>
 								</div>
 
 							`;
 							}).join('');
-							document.querySelector('.rowling-fixture').innerHTML = fixture;		
+							document.querySelector('.rowling-fixture').innerHTML = fixture;
+
+							// conditions for live fixtures
+							let score = document.querySelectorAll('.home-score-live');
+							score.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
+							let score2 = document.querySelectorAll('.away-score-live');
+							score2.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
 						})
 }
-
+getLiveFixtures();
 
 // finished fixtures
 function getFinishedFixtures() {
@@ -132,6 +207,82 @@ function getFinishedFixtures() {
 		.then(data => {
 			let fixtures = data['response']
 			let fixture = fixtures.map(data => {
+
+				// converting timestamp to local time
+				let date = new Date(data.fixture.timestamp * 1000);
+				let hours = date.getHours();
+				let minutes = "0" + date.getMinutes();
+				let formattedTime = hours + ':' + minutes.substr(-2);
+
+				// add am or pm
+				let ampm = hours >= 12 ? 'pm' : 'am';
+				formattedTime += ampm;
+				            return `
+								<div class="col-md-12 col-12 cursour">
+									<div class="d-flex justify-content-between align-items-center my-1">
+										<div class="d-flex justify-content-start align-items-center my-1">
+											<img src="${data.teams.home.logo}" alt="" class="home-logo" width="30px">
+											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.home.name}</h5>
+										</div>	
+										<h5 class="home-score-live text-dark" style="font-size: 13px">${data.goals.home}</h5>
+									</div>
+								</div>
+								<div class="col-md-12 col-12">
+									<div class="d-flex justify-content-between align-items-center my-0">
+										<p class="time text-danger my-0 mx-3" style="font-size: 10px">${formattedTime}</p>
+										<p class="time text-danger my-0 mx-3" style="font-size: 11px">${data.fixture.status.short}</p>
+									</div>
+								</div>
+								<div class="col-md-12 col-12  cursour border-bottom border-3">
+									<div class="d-flex justify-content-between align-items-center my-1">
+										<div class="d-flex justify-content-start align-items-center my-1">
+											<img src="${data.teams.away.logo}" alt="" class="home-logo" width="30px">
+											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.away.name}</h5>
+										</div>	
+										<h5 class="away-score-live text-dark" style="font-size: 13px">${data.goals.away}</h5>
+									</div>
+								</div>
+
+							`;
+							}).join('');
+							document.querySelector('.rowling-fixture').innerHTML = fixture;		
+
+							// if score is null then display ? 
+							let score = document.querySelectorAll('.home-score-live');
+							score.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
+							let score2 = document.querySelectorAll('.away-score-live');
+							score2.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '-';
+								}
+							}
+							);
+						})
+}
+
+// get upcoming fixtures
+function getNextFixtures() {
+	
+	fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures?next=50', options)
+		.then(response => response.json())
+		.then(data => {
+			let fixtures = data['response']
+			let fixture = fixtures.map(data => {
+
+				// converting timestamp to local time
+				let date = new Date(data.fixture.timestamp * 1000);
+				let hours = date.getHours();
+				let minutes = "0" + date.getMinutes();
+				let formattedTime = hours + ':' + minutes.substr(-2);
+
+				// add am or pm
+				let ampm = hours >= 12 ? 'pm' : 'am';
+				formattedTime += ampm;
 				            return `
 								<div class="col-md-12 col-12 cursour">
 									<div class="d-flex justify-content-between align-items-center my-1">
@@ -140,6 +291,12 @@ function getFinishedFixtures() {
 											<h5 class="home text-light mx-3" style="font-size: 13px">${data.teams.home.name}</h5>
 										</div>	
 										<h5 class="home-score-live text-light" style="font-size: 13px">${data.goals.home}</h5>
+									</div>
+								</div>
+								<div class="col-md-12 col-12">
+									<div class="d-flex justify-content-between align-items-center my-0">
+										<p class="time text-danger my-0 mx-3" style="font-size: 10px">${formattedTime}</p>
+										<p class="time text-danger my-0 mx-3" style="font-size: 11px">${data.fixture.status.short}</p>
 									</div>
 								</div>
 								<div class="col-md-12 col-12  cursour border-bottom border-3">
@@ -154,14 +311,26 @@ function getFinishedFixtures() {
 
 							`;
 							}).join('');
-							document.querySelector('.rowling-fixture').innerHTML = fixture;		
+							document.querySelector('.rowling-fixture').innerHTML = fixture;
+						
+							// if score is null then display ? 
+							let score = document.querySelectorAll('.home-score-live');
+							score.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '?';
+								}
+							}
+							);
+							let score2 = document.querySelectorAll('.away-score-live');
+							score2.forEach(score => {
+								if (score.innerHTML === 'null') {
+									score.innerHTML = '?';
+								}
+							}
+							);
+
 						})
 }
-
-
-
-
-
 
 // switch between fixtures and live fixtures
 function switchFixtures() {
@@ -174,8 +343,8 @@ function switchFixtures() {
 	document.querySelector('.finished').addEventListener('click', function() {
 		getFinishedFixtures();
 	});
+	document.querySelector('.schedule').addEventListener('click', function() {
+		getNextFixtures();
+	});
 }
 switchFixtures();
-
-// fixtures conditions
-
